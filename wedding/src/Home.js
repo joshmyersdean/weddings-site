a072acc36f87e494af30011dda2a21b06f0cd739
Josh Myers-Dean <myersdj@wwu.edu>
2019-09-17 08:31:02 -0700

    Pls work

---
+++ wedding/src/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+function Home() {
+    return (
+        <div className="App">
+        <div className="App-header">
+        <img src={logo} className="app-logo" alt="logo" />
+        <h2>Josh and Julia&#39;s Wedding!</h2>
+        </div>
+        </div>
+    );
+}
+
+export default Home;
