f1d5e70fe2acb553f45decda8227070302aaba9a
Josh Myers-Dean <joshmyersdean@yahoo.com>
2019-12-16 16:30:55 -0800

    Dependency update and minor updates

--- src/pages/WeddingDay.js
+++ src/pages/WeddingDay.js
@@ -4,6 +4,8 @@ import PageWithNav from "./helpers/PageWithNav";
 import { H5, B2, H6 } from "../components/Fonts/Fonts";
 import { HOME, AFTER_PARTY, RSVP_ROUTE } from "../routes/routes";
 import { Nav, Navbar } from "react-bootstrap";
+
+
 const HeadingContainer = styled.section`
   margin-bottom: 1rem;
 `;
@@ -49,8 +51,8 @@ export default () => (
   <PageWithNav>
     <Fragment>
       <HeadingContainer>
-        <MainHeader>20th June 2020</MainHeader>
-        <VenueInfo>Millersylvania State Park Retreat Center</VenueInfo>
+        <MainHeader>18th September 2020</MainHeader>
+        <VenueInfo>Excelsior Group Campground</VenueInfo>
         <B2>
           Our wedding ceremony will take place at x time at y place and the
           party will begin at 5pm at z place.
@@ -63,27 +65,14 @@ export default () => (
         style cabins that can hold roughly 144 people, camping is also available
         in the park for those who would like a bit more privacy. For those who
         do not wish to camp or bunk there are plently of{" "}
-        <a href="https://www.priceline.com/relax/in/Olympia,WA,US/rooms/1/?adp=1t4&city_id=3000023353&gclid=EAIaIQobChMI2_6YkcrY5AIV-R6tBh1nJQESEAAYBCAAEgKRZPD_BwE&kw=olympia%20wa%20hotels&match=e&query=olympia%20wa%20hotels&refclickid=d%3Achotel14935375410g2705515167601402377001kwd-134496855%7C9033356%7C1t4&refid=PLGOOGLECPC&slingshot=1749&statename=Washington">
+        <a href="https://www.priceline.com/relax/in/Bellingham,%20WA/rooms/1">
           hotels
         </a>{" "}
         in and around Olympia.
       </P>
 
       <SectionTitle>Directions</SectionTitle>
-      <DirectionsContainer>
-        <DirectionsFrame
-          title="Directions"
-          width="300"
-          height="300"
-          src={
-            "https://www.google.com/maps/embed/v1/place?key=" +
-            process.env.REACT_APP_API_KEY +
-            "&q=millersylvania+environmental+learning+center"
-          }
-          frameborder="0"
-          scrolling="no"
-        />
-      </DirectionsContainer>
+      // @TODO
 
       <SectionTitle>Details</SectionTitle>
       <P>
